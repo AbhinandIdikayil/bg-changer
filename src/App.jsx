3dea93ce56f91f5daf7035551e5bdddf0e666154
Abhinand <abhin4nd6@gmail.com>
2024-04-07 17:19:31 +0530

    using state instead of useRed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState,useRef } from 'react'
+import { useState, useRef } from 'react'
 
 function App() {
   const [color, setColor] = useState('olive');
   const [isChanging, SetIsChanging] = useState(false);
-  const intervalIdRef = useRef(null);
+  const intervalidRef = useRef(null);
+  const [intervalId,setIntervalId] = useState(null);
 
   const colors = ['red', 'blue', 'green', 'brown', 'yellow', 'black', 'orange'];
   function changeBgColor(color) {
@@ -12,17 +13,22 @@ function App() {
 
   function startPreview() {
     let index = 0;
-    intervalIdRef.current = setInterval(() => {
-      changeBgColor(colors[index])
-      index = parseInt((index + 1) % colors.length); // Update index and loop back to 0 if it reaches the end
-    }, 1000);
+    // intervalidRef.current = setInterval(() => {
+    //   changeBgColor(colors[index])
+    //   index = parseInt((index + 1) % colors.length); // Update index and loop back to 0 if it reaches the end
+    // }, 1000);
+    setIntervalId(setInterval(() => {
+        changeBgColor(colors[index])
+        index = parseInt((index + 1) % colors.length); // Update index and loop back to 0 if it reaches the end
+      }, 1000))
   }
 
   function handlePreviewClick() {
     if (isChanging) {
-      clearInterval(intervalIdRef.current);
+      // clearInterval(intervalidRef.current);
+      clearInterval(intervalId)
       SetIsChanging(false)
-    }else{
+    } else {
       SetIsChanging(true)
       startPreview()
     }
@@ -34,14 +40,14 @@ function App() {
       <div className='w-full h-screen duration-700' style={{ backgroundColor: color }}>
         <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
           <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-            {colors.map((elem,i) => {
+            {colors.map((elem, i) => {
               return (
                 <button key={i} onClick={() => changeBgColor(elem)} style={{ backgroundColor: elem }} className='outline-none font-bold px-4 py-1 rounded-full text-zinc-50 shadow-lg'> {elem} </button>
               )
             })}
             <button onClick={handlePreviewClick} style={{ backgroundColor: "#FF512F" }} className='outline-none font-bold px-4 py-1 rounded-full text-zinc-50 shadow-lg'>
               {isChanging ? '.stop.' : 'preview'}
-              </button>
+            </button>
           </div>
         </div>
       </div>
